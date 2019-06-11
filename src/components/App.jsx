d7defa9cfd131515c3dcf62c1de0308e17d95477
littlejennie <jenniezeng1@gmail.com>
2019-06-10 21:05:22 -0700

    Video onClick initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,78 @@
 import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
 import exampleVideoData from '../data/exampleVideoData.js';
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+// var App = () => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <div><h5><em>search</em> view goes here</h5></div>
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <VideoPlayer
+//           video = {exampleVideoData[0]}
+//         />
+//       </div>
+//       <div className="col-md-5">
+//         <VideoList 
+//           videos = {exampleVideoData}
+//         />
+//       </div>
+//     </div>
+//   </div>
+// );
+
+
+//refactor to ES6
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      //done: false,
+      videos: exampleVideoData, // this is an array with video objects
+      playingVideo: '' // this is a single video object for VideoPlayer to use
+    }
+    this.onTitleClick = this.onTitleClick.bind(this);
+  }
+  onTitleClick() {
+    this.setState({
+      // we will need to reset playingVideo to the clicked video from videoListEntries
+      // we will grab it based on the etag from click
+      // and set the playingVideo to such etag video
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer
+              video = {exampleVideoData[0]}
+            />
+          </div>
+        </div>
+        <div className="col-md-5">
+          <VideoList 
+            videos = {this.state.videos}
+            clickHandler = {this.onTitleClick}
+          />
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer
-          video = {exampleVideoData[0]}
-        />
-      </div>
-      <div className="col-md-5">
-        <VideoList 
-          videos = {exampleVideoData}
-        />
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -5,7 +5,7 @@ var VideoList = (props) => (
   <div className="video-list">
     {
       props.videos.map(video => (
-        <VideoListEntry key={video.id.videoId} video={video} />
+        <VideoListEntry key={video.id.videoId} video={video} clickHandler={props.clickHandler}/>
       ))
     }
   </div>
