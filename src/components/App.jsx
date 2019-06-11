0a2e9b0ebe72090bd5ec3ecb6168b926ac381da9
littlejennie <jenniezeng1@gmail.com>
2019-06-11 15:10:13 -0700

    get method initialized

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
+import Search from './Search.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 // var App = () => (
 //   <div>
@@ -67,7 +68,7 @@ class App extends React.Component {
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em>search</em> view goes here</h5></div>
+            <Search />
           </div>
         </nav>
         <div className="row">
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,46 @@
-var Search = () => (
-  <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
-    <button className="btn hidden-sm-down">
-      <span className="glyphicon glyphicon-search"></span>
-    </button>
-  </div> 
-);
+import YOUTUBE_API_KEY from "../config/youtube.js";
+
+// var Search = () => (
+//   <div className="search-bar form-inline">
+//     <input className="form-control" type="text" />
+//     <button className="btn hidden-sm-down">
+//       <span className="glyphicon glyphicon-search"></span>
+//     </button>
+//   </div> 
+// );
+
+
+
+class Search extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  get(searchText, successCB, errorCB=null) {
+    $.get(
+      "https://www.googleapis.com/youtube/v3/search",
+      {
+        part: "snippet",
+        key: YOUTUBE_API_KEY,
+        maxResults: 5,
+        q: searchText
+      },
+      successCB(data)
+    );
+  }
+
+  render() {
+    return (
+      <div className="search-bar form-inline">
+        <input className="form-control" type="text" />
+        <button className="btn hidden-sm-down">
+          <span className="glyphicon glyphicon-search"></span>
+        </button>
+      </div> 
+    );
+  }
+
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
